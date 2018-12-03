aa773ee862ce4e2db6fee7ec01df249a83d6672f
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-12-04 01:05:41 +0200

    POST_GET

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const data = 'Hi!'
-app.get('/', (req, res) => res.render('index', {data}))
+const arr = ['hello1', 'world', 'test']
+
+app.get('/', (req, res) => res.render('index', {arr: arr}))
+ 
+app.get('/create', (req, res) => res.render('create'))
+app.post('/create', (req, res) => {
+    arr.push(req.body.text)
+    res.redirect('/')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
