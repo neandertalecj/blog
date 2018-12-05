302caef71e32bbe97260c9705f40f7de68bb32d0
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-12-05 14:50:02 +0200

    Created first model (schme)

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const Post = require('./models/post')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const arr = ['hello', 'world', 'test']
-
-app.get('/', (req, res) => res.render('index', { arr: arr }))
+app.get('/', (req, res) => {
+  Post.find({}).then(posts => {
+    res.render('index', { posts })
+  })
+})
 
 app.get('/create', (req, res) => res.render('create'))
 app.post('/create', (req, res) => {
-  arr.push(req.body.text)
+  const { title, body } = req.body
+
+  Post.create({
+    title,
+    body
+  }).then(post => console.log(post.id))
+
   res.redirect('/')
 })
 
